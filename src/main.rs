1f2fd1bfdc7bda873d2a12cded92f5ffd9700a00
SaxsCode <maxsax.sch@gmail.com>
2025-06-21 23:11:42 +0200

    feat: let application run for longer time

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
-extern crate winrt_notification;
 use winrt_notification::{Duration, Sound, Toast};
+use std::thread::sleep;
+use std::time::Duration as StdDuration;
 
 fn main() {
+    let mut second:u8 = 0;
+    while second < 60 {
+        if second == 10 {
+            send_alert();
+        }
+        sleep(StdDuration::from_secs(1));
+        second+=1;
+    }
+}
+
+fn send_alert() {
     Toast::new(Toast::POWERSHELL_APP_ID)
         .title("Time to take a seat!")
         .sound(Some(Sound::SMS))
