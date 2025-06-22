ce3e5f52f3f34e71c5b6a776226ef8e0fba68764
SaxsCode <maxsax.sch@gmail.com>
2025-06-22 09:28:31 +0200

    feat: read settings from json file and print them

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
-use winrt_notification::{Duration, Sound, Toast};
+use serde::Deserialize;
+use std::collections::HashMap;
 use std::thread::sleep;
 use std::time::Duration as StdDuration;
+use winrt_notification::{Duration, Sound, Toast};
+use std::fs;
+
+#[derive(Debug, Deserialize)]
+struct WorkTime {
+    title: String,
+    range: Vec<String>,
+    active: u8,
+}
 
 fn main() {
-    let mut second:u8 = 0;
+    let file_content = fs::read_to_string("src/worktimes.json")
+        .expect("Failed to read");
+
+    let response: Vec<WorkTime> = serde_json::from_str(&file_content)
+        .expect("Failed to parse");
+
+    println!("{response:#?}");
+
+    let mut second: u8 = 0;
     while second < 60 {
         if second == 10 {
             send_alert();
         }
         sleep(StdDuration::from_secs(1));
-        second+=1;
+        second += 1;
     }
 }
 
